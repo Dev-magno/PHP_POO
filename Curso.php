56946f7ab8add99361134c3e0f5b5ae2509cde73
Dev-magno <magnolopes1212@gmail.com>
2024-07-17 17:54:40 -0300

    crud iniciado

--- Curso.php
+++ Curso.php
@@ -31,12 +31,3 @@ $minhaOperacao->dividir();
 $minhaOperacao->resultado();
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <main>
--- index.php
+++ index.php
@@ -1,51 +1,35 @@
-<?php 
-require_once '_cabecalho.php'
-?>
-<h1>Moto</h1>
-<form action="">
-    <label for="">Marca</label>
-    <input type="text" name="" id="">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="divi-form">
+        <h1>CRUD de cursos</h1>
+        <form action="adicionar_curso.php">
+            <label for="curso">Curso</label>
+            <input type="text" name="curso" id="curso" require>
+            
+            <label for="descrição">Descrição</label>
+            <textarea id="descricao" name="descricao" rows="4" required>Aprenda programação em diversas linguagens.</textarea>
 
-    <label for="">Modelo</label>
-    <input type="text" name="" id="">
+            <label for="cargahoraria">Carga Horária</label>
+            <input type="number" name="cargahoraria" id="cargahoraria" require>
 
-    <label for="">Tipo</label>
-    <input type="text" name="" id="">
-</form>
+            <label for="professor">Professor</label>
+            <input type="text" name="professor" id="professor" require>
+            <input type="submit" value="Adicionar Curso">
+        </form>
+    </div>
 
-<?php 
-require_once '_rodape.php'
-?>
-
-
-class Moto{
-    public $marca;
-    public $modelo;
-    public $tipo;
-
-    public function ligar(){
-        echo "Dando partida na minha $this->marca $this->modelo $this->tipo<br>";
-    }
-
-
-    public function pilotar(){
-        echo "Pilotando minha $this->marca $this->modelo $this->tipo<br>";
-    }
-
-    public function desligar(){
-        echo "Desligando minha $this->marca $this->modelo $this->tipo<br>";
-    }
-
-    public function estacionar(){
-        echo "Estacionar minha $this->marca $this->modelo $this->tipo<br>";
-    }
-}
-
-$minhaMoto = new Moto();
-$minhaMoto->marca = "Honda";
-$minhaMoto->marca = "Yamaha";
-$minhaMoto->tipo = "Lander";
-$minhaMoto->ligar();
-$minhaMoto->pilotar();
-$minhaMoto->desligar();
-$minhaMoto->estacionar();
+    <div id="listar">
+        <h2>Listar Crusos</h2>
+        <div id="listar-curso">
+            <?php include 'listar_curso.php' ?>
+        </div>
+    </div>
+</body>
+</html>
